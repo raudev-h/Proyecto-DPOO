caaa924a7dc8a0994f76088267833f7e73a5d343
raul <raulalbertohechavarria@gmail.com>
2025-05-20 10:15:36 -0400

    se crearon las clases CuentaNauta y MesDatos, además también modifique el gitignore

---
+++ src/logica/CuentaNauta.java
@@ -0,0 +1,22 @@
+package logica;
+
+public class CuentaNauta {
+    private String nick;
+
+    // Constructor
+    public CuentaNauta(String nick) {
+        setNick(nick);
+    }
+
+    // Getters y setters
+    public String getNick() {
+        return nick;
+    }
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+    
+    
+
+    
+}
