94e9c19d366f726e966274a82e3bbfd909017f17
Aniel.py <anielvarela64@gmail.com>
2025-06-24 19:36:58 -0400

    Ajustes

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -27,23 +27,6 @@ public class Principal extends JFrame {
     private JPanel contentPane;
     private String imagenFondoActual = "/imagenes/d.png"; // Imagen por defecto
     
-    
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Inicializadora.Inicializar();
-                    Principal frame = new Principal();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 
     /**
      * Create the frame.
